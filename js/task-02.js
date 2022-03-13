254df6d082e8c24cdae349ef4294a2eaa0a85449
kateryna klimchuk <ketyclima@gmail.com>
2022-03-13 15:57:57 +0200

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients')
+
+const createListItem = options => {
+
+  return options.map(option => {
+    const element = document.createElement('li');
+    element.classList.add('item');
+    element.textContent = option;
+    
+    return element;
+  })
+};
+
+const elements = createListItem(ingredients);
+
+ingredientsEl.append(...elements);
