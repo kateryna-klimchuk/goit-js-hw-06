ae9140b3dce4801e30cdcfe406f240150dc74080
kateryna klimchuk <ketyclima@gmail.com>
2022-03-15 17:59:37 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const textEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
+const onButtonClick = () => {
+  console.log(textEl.textContent);
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  textEl.textContent = bodyEl.style.backgroundColor;
+}
+
+buttonEl.addEventListener('click', onButtonClick);
