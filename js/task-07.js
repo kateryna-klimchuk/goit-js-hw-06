b96221b3e88c36c7b5840b699752f9ce617f224f
kateryna klimchuk <ketyclima@gmail.com>
2022-03-15 17:35:25 +0200

    task-8

--- js/task-07.js
+++ js/task-07.js
@@ -5,7 +5,6 @@ const textEl = document.querySelector('#text');
 
 const onRangeChange = () => {
     textEl.style.fontSize = rangeEl.value + 'px';
-    console.log(textEl.style.fontSize);
 }
 
 rangeEl.addEventListener('input', onRangeChange)
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
 
+const form = document.querySelector('.login-form');
+
+const onSubmitForm = (event) => {
+    event.preventDefault();
+
+    const {
+    elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+    return alert('Please, fill all fields!');
+    }
+
+    console.log(`client:
+    email: ${email.value}, password: ${password.value}`);
+    event.currentTarget.reset();
+}
+
+
+form.addEventListener('submit', onSubmitForm)
